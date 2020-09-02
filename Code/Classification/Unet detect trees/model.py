2159ec8b9a857cee464d3d7fc1c4ad3b6a9eac95
Gini <ginihumer96@gmail.com>
2020-09-02 20:51:21 +0200

    init ML code

---
+++ Code/Classification/Unet detect trees/model.py
@@ -0,0 +1,353 @@
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+
+import tensorflow as tf
+from keras import backend as K
+from keras.losses import binary_crossentropy
+
+
+# https://github.com/keras-team/keras/issues/9395
+def generalized_dice_coeff(y_true, y_pred):
+    Ncl = y_pred.shape[-1]
+    w = K.zeros(shape=(Ncl,))
+    w = K.sum(y_true, axis=(0,1,2))
+    w = 1/(w**2+0.000001)
+    # Compute gen dice coef:
+    numerator = y_true*y_pred
+    numerator = w*K.sum(numerator,(0,1,2,3))
+    numerator = K.sum(numerator)
+
+    denominator = y_true+y_pred
+    denominator = w*K.sum(denominator,(0,1,2,3))
+    denominator = K.sum(denominator)
+
+    gen_dice_coef = 2*numerator/denominator
+
+    return gen_dice_coef
+
+def generalized_dice_loss(y_true, y_pred):
+    return 1 - generalized_dice_coeff(y_true, y_pred)
+
+def dice_coef_binary_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - dice_coef(y_true, y_pred)
+
+def dice_coef(y_true, y_pred):
+    smooth = 1e-7 #1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+#https://www.kaggle.com/mauddib/data-science-bowl-tutorial-using-cnn-tensorflow/comments
+def bce_dice_loss(y_true, y_pred):
+    return 0.5 * binary_crossentropy(y_true, y_pred) - dice_coef(y_true, y_pred)
+
+# Ref: salehi17, "Twersky loss function for image segmentation using 3D FCDN"
+# -> the score is computed for each class separately and then summed
+# alpha=beta=0.5 : dice coefficient
+# alpha=beta=1   : tanimoto coefficient (also known as jaccard)
+# alpha+beta=1   : produces set of F*-scores
+# implemented by E. Moebel, 06/04/18
+def tversky_loss(y_true, y_pred):
+    alpha = 0.5
+    beta  = 0.5
+    
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred      # proba that voxels are class i
+    p1 = ones-y_pred # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones-y_true
+    
+    num = K.sum(p0*g0, (0,1,2,3))
+    den = num + alpha*K.sum(p0*g1,(0,1,2,3)) + beta*K.sum(p1*g0,(0,1,2,3))
+    
+    T = K.sum(num/den) # when summing over classes, T has dynamic range [0 Ncl]
+    
+    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
+    return Ncl-T
+
+#https://lars76.github.io/neural-networks/object-detection/losses-for-segmentation/
+#https://www.kaggle.com/c/ultrasound-nerve-segmentation/discussion/22361
+def balanced_bce_loss(y_true, y_pred):
+    class_weights = [1./80., 1.]
+    return -(class_weights[1] * y_true * K.log(y_pred) + class_weights[0] * (1.0 - y_true) * K.log(1.0 - y_pred))
+
+    # where
+    # class_weights[0] = 1. / 80.
+    # class_weights[1] = 1.
+
+    
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+from keras.activations import softmax
+
+def softMaxAxis1(x):
+    return softmax(x,axis=3)
+
+#https://github.com/keras-team/keras/issues/6261
+def pixelwise_crossentropy(target, output):
+    weights = [1,1,1]
+    output = tf.clip_by_value(output, 10e-8, 1. - 10e-8)
+    return - tf.reduce_sum(target * weights * tf.math.log(output))
+
+#https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    
+    weights = K.variable(weights)
+        
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+    
+    return loss
+
+
+    
+def unet(pretrained_weights = None,input_size = (256,256,1), nr_classes = 3):
+    i = Input(input_size)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(i)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    #conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9) # for binary segmentation
+    conv9 = Conv2D(32, 3, activation='relu', padding = 'same', kernel_initializer = 'he_normal')(conv9) # for multi-label segmentation
+    #conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9) # for binary segmentation
+    conv10 = Conv2D(nr_classes, (1, 1), activation = softMaxAxis1)(conv9) # for multi-label segmentation
+    
+    model = Model(inputs = i, outputs = conv10)
+
+    #model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = [tf.keras.metrics.MeanIoU(num_classes=2)])#['accuracy'])
+    #model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['acc']) # for binary segmentation #,f1_m,precision_m, recall_m,tf.keras.metrics.MeanIoU(num_classes=2)
+    #weights = np.ones(nr_classes)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'categorical_crossentropy', metrics = ['accuracy', tf.keras.metrics.MeanIoU(num_classes=nr_classes)]) # for multi-label segmentation
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+
+##-----FC Dense Net---
+# adapted from: https://github.com/SimJeg/FC-DenseNet
+
+def BN_ReLU_Conv(inputs, n_filters, filter_size=3, dropout_p=0.2):
+    """
+    Apply successivly BatchNormalization, ReLu nonlinearity, Convolution and Dropout (if dropout_p > 0) on the inputs
+    """
+    
+    l = BatchNormalization()(inputs)
+    l = ReLU()(l)
+    l = Conv2D(n_filters, filter_size, padding="same", kernel_initializer='he_uniform')(l)
+    l = Dropout(rate=dropout_p)(l)
+    return l
+
+def TransitionDown(inputs, n_filters, dropout_p=0.2):
+    """ Apply first a BN_ReLu_conv layer with filter size = 1, and a max pooling with a factor 2  """
+
+    l = BN_ReLU_Conv(inputs, n_filters, filter_size=1, dropout_p=dropout_p)
+    l = MaxPooling2D(pool_size=(2, 2))(l)
+
+    return l
+    # Note : network accuracy is quite similar with average pooling or without BN - ReLU.
+    # We can also reduce the number of parameters reducing n_filters in the 1x1 convolution
+
+def TransitionUp(skip_connection, block_to_upsample, n_filters_keep):
+    """
+    Performs upsampling on block_to_upsample by a factor 2 and concatenates it with the skip_connection """
+
+    # Upsample
+    l = Concatenate()(block_to_upsample)
+    l = Conv2DTranspose(filters=n_filters_keep, kernel_size=3, strides=2, padding='same', kernel_initializer='he_uniform')(l)
+    # Concatenate with skip connection
+    l = Concatenate()([l, skip_connection])
+
+    return l
+    # Note : we also tried Subpixel Deconvolution without seeing any improvements.
+    # We can reduce the number of parameters reducing n_filters_keep in the Deconvolution
+    
+def SoftmaxLayer(inputs, n_classes):
+    """
+    Performs 1x1 convolution followed by softmax nonlinearity
+    The output will have the shape (batch_size  * n_rows * n_cols, n_classes)
+    """
+
+    l = Conv2D(n_classes, kernel_size=1, kernel_initializer='he_uniform', padding='same', activation = softMaxAxis1)(inputs)
+
+    # We perform the softmax nonlinearity in 2 steps :
+    #     1. Reshape from (batch_size, n_classes, n_rows, n_cols) to (batch_size  * n_rows * n_cols, n_classes)
+    #     2. Apply softmax
+
+#     _, n_rows, n_cols, _ = l.shape
+#     size = int(n_rows * n_cols)
+#     l = Reshape((size, n_classes))(l)
+#     l = Softmax()(l)
+    return l
+
+    # Note : we also tried to apply deep supervision using intermediate outputs at lower resolutions but didn't see
+    # any improvements. Our guess is that FC-DenseNet naturally permits this multiscale approach 
+
+def fc_dense_net(pretrained_weights = None,input_size = (512,512,3), nr_classes = 4, n_filters_first_conv=48, n_pool=4, growth_rate=12, n_layers_per_block=5, dropout_p=0.2):
+    
+    inputs = Input(input_size)
+    stack = Conv2D(n_filters_first_conv, kernel_size=3, strides=(2, 2), padding="same", kernel_initializer='he_uniform')(inputs)
+    n_filters = n_filters_first_conv
+    
+    #####################
+    # Downsampling path #
+    #####################
+
+    skip_connection_list = []
+    for i in range(n_pool):
+        # Dense Block
+        for j in range(n_layers_per_block):#[i]):
+            # Compute new feature maps
+            l = BN_ReLU_Conv(stack, growth_rate, dropout_p=dropout_p)
+            # And stack it : the Tiramisu is growing
+            stack = Concatenate()([stack, l])
+            n_filters += growth_rate
+        # At the end of the dense block, the current stack is stored in the skip_connections list
+        skip_connection_list.append(stack)
+
+        # Transition Down
+        stack = TransitionDown(stack, n_filters, dropout_p)
+
+    skip_connection_list = skip_connection_list[::-1]
+    
+    
+    #####################
+    #     Bottleneck    #
+    #####################
+
+    # We store now the output of the next dense block in a list. We will only upsample these new feature maps
+    block_to_upsample = []
+
+    # Dense Block
+    for j in range(n_layers_per_block):#[n_pool]):
+        l = BN_ReLU_Conv(stack, growth_rate, dropout_p=dropout_p)
+        block_to_upsample.append(l)
+        stack = Concatenate()([stack, l])
+
+        
+    #######################
+    #   Upsampling path   #
+    #######################
+
+    for i in range(n_pool):
+        # Transition Up ( Upsampling + concatenation with the skip connection)
+        n_filters_keep = growth_rate * n_layers_per_block#[n_pool + i]
+        stack = TransitionUp(skip_connection_list[i], block_to_upsample, n_filters_keep)
+
+        # Dense Block
+        block_to_upsample = []
+        for j in range(n_layers_per_block):#[n_pool + i + 1]):
+            l = BN_ReLU_Conv(stack, growth_rate, dropout_p=dropout_p)
+            block_to_upsample.append(l)
+            stack = Concatenate()([stack, l])
+
+
+    #####################
+    #      Softmax      #
+    #####################
+
+    output_layer = SoftmaxLayer(stack, nr_classes)
+
+    model = Model(inputs, output_layer)
+    
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'categorical_crossentropy', metrics = ['accuracy', tf.keras.metrics.MeanIoU(num_classes=nr_classes)]) # for multi-label 
+    
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
